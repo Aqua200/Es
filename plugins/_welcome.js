62fd1089fb606fd5b963c428eab39208b325d925
Aqua200 <chinquepapa@gmail.com>
2025-02-07 08:13:15 -0800

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -10,8 +10,9 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
 
   // Asegúrate de que global.welcom1 y global.welcom2 estén definidos
-  let welcomeMessage = global.welcom1 || '¡Bienvenido al grupo!'
-  let goodbyeMessage = global.welcom2 || '¡Te extrañaremos!'
+  let welcomeMessage = global.welcom1 || '¡Bienvenido al grupo, que alegría tenerte aquí!'
+  let goodbyeMessage = global.welcom2 || 'Nos da tristeza verte ir, ¡esperamos verte de nuevo pronto!'
+  let removedMessage = global.welcom3 || 'Parece que alguien ha sido eliminado... ¡Esperamos que todo esté bien!'
 
   if (chat.welcome) {
     let img;
@@ -23,11 +24,14 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `✦ *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${welcomeMessage}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      let bienvenida = `🌸 *Bienvenido* a ${groupMetadata.subject}\n ✨ ${taguser}\n${welcomeMessage}\n •(=^●ω●^=)• ¡Disfruta mucho de tu estadía y no dudes en interactuar!\n> ✐ Usa *#help* para conocer todos los comandos disponibles.`
       await conn.sendMessage(m.chat, { text: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `✦ *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${goodbyeMessage}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      let bye = `🌙 *Adiós* de ${groupMetadata.subject}\n ✨ ${taguser}\n${goodbyeMessage}\n •(=^●ω●^=)• ¡Te esperamos con los brazos abiertos la próxima vez!\n> ✐ No olvides que puedes usar *#help* para ver todos los comandos cuando regreses.`
       await conn.sendMessage(m.chat, { text: bye, mentions: [who] })
+    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_KICK) {
+      let removed = `💨 *Eliminado* de ${groupMetadata.subject}\n ✨ ${taguser}\n${removedMessage}\n •(=^●ω●^=)• ¡Que todo esté bien, te esperamos si decides regresar!\n> ✐ Recuerda usar *#help* cuando regreses para ver los comandos.`
+      await conn.sendMessage(m.chat, { text: removed, mentions: [who] })
     }
   }
 
