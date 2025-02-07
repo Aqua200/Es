811196ac7a4be25ef30630d02cf18882d26794dd
Aqua200 <chinquepapa@gmail.com>
2025-02-07 08:17:24 -0800

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -9,11 +9,6 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let chat = global.db.data.chats[m.chat]
   let defaultImage = 'https://files.catbox.moe/xr2m6u.jpg';
 
-  // Asegúrate de que global.welcom1 y global.welcom2 estén definidos
-  let welcomeMessage = global.welcom1 || '¡Bienvenido al grupo, que alegría tenerte aquí!'
-  let goodbyeMessage = global.welcom2 || 'Nos da tristeza verte ir, ¡esperamos verte de nuevo pronto!'
-  let removedMessage = global.welcom3 || 'Parece que alguien ha sido eliminado... ¡Esperamos que todo esté bien!'
-
   if (chat.welcome) {
     let img;
     try {
@@ -24,14 +19,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `🌸 *Bienvenido* a ${groupMetadata.subject}\n ✨ ${taguser}\n${welcomeMessage}\n •(=^●ω●^=)• ¡Disfruta mucho de tu estadía y no dudes en interactuar!\n> ✐ Usa *#help* para conocer todos los comandos disponibles.`
-      await conn.sendMessage(m.chat, { text: bienvenida, mentions: [who] })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `🌙 *Adiós* de ${groupMetadata.subject}\n ✨ ${taguser}\n${goodbyeMessage}\n •(=^●ω●^=)• ¡Te esperamos con los brazos abiertos la próxima vez!\n> ✐ No olvides que puedes usar *#help* para ver todos los comandos cuando regreses.`
-      await conn.sendMessage(m.chat, { text: bye, mentions: [who] })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_KICK) {
-      let removed = `💨 *Eliminado* de ${groupMetadata.subject}\n ✨ ${taguser}\n${removedMessage}\n •(=^●ω●^=)• ¡Que todo esté bien, te esperamos si decides regresar!\n> ✐ Recuerda usar *#help* cuando regreses para ver los comandos.`
-      await conn.sendMessage(m.chat, { text: removed, mentions: [who] })
+      let bienvenida = `✦ *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
+      let bye = `✦ *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ✐ Puedes usar *#help* para ver la lista de comandos.`
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
   }
 
