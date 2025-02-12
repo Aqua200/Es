5738e6464bbd56fe4d284cf11ae37bd005fcedb2
Aqua200 <chinquepapa@gmail.com>
2025-02-11 23:59:48 -0800

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN: Creador Anika;;\nFN:Creador Anika\nORG:Creador Anika\nTITLE:Creador de Anika\nitem1.TEL;waid=526631079388:526631079388\nitem1.X-ABLabel:creador anika\nitem2.EMAIL;type=INTERNET:creador@anika.com\nitem2.X-ABLabel:Correo Electrónico\nitem3.URL:http://www.Anika.com\nitem3.X-ABLabel:Sitio Web\nX-WA-BIZ-DESCRIPTION:Anika Bot es un bot de WhatsApp que ofrece diversas funciones para automatización y entretenimiento.\nX-WA-BIZ-NAME:Creador Anika\nEND:VCARD`
+    let vcard = `BEGIN:VCARD\nVERSION:3.0\nN: Creador Anika;;\nFN:Creador Anika\nORG:Creador Anika\nTITLE:Creador de Anika\nitem1.TEL;waid=526631079388:526631079388\nitem1.X-ABLabel:creador anika\nitem2.EMAIL;type=INTERNET:creador@anika.com\nitem2.X-ABLabel:Correo Electrónico\nitem3.URL:http://www.anika.com\nitem3.X-ABLabel:Sitio Web\nitem4.URL:https://www.anika.com/creadoranika\nitem4.X-ABLabel:Instagram\nX-WA-BIZ-DESCRIPTION:anika Bot es un bot de WhatsApp que ofrece diversas funciones como respuestas automáticas, comandos personalizados y entretenimiento. ¡Interacción fácil y rápida!\nX-WA-BIZ-NAME:Creador Anika\nEND:VCARD`
     
     // Enviar el mensaje con los detalles
     await conn.sendMessage(m.chat, {
