1f2a2d8fb7cef6f2aacef9f782118156670d9e0f
Aqua200 <chinquepapa@gmail.com>
2025-02-12 08:26:54 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,70 +1,181 @@
-import fs, { watchFile, unwatchFile } from 'fs'
+import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
-import cheerio from 'cheerio'
+import fs from 'fs'
+import * as cheerio from 'cheerio'  // Corregido
 import fetch from 'node-fetch'
 import axios from 'axios'
-import moment from 'moment-timezone'
+import moment from 'moment-timezone' 
+
+//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─⋯
+
+global.botNumberCode = '' //Ejemplo +5216671548328
+global.confirmCode = ''
+
+//*─ׄ─⋯
 
 global.owner = [
-  ['5216631079388', '@Neykoor⛩️', true], // Número corregido
-  ['521XXXXXXXXXX', '@Three✨', true],
-]
+  ['5216631079388', 'Neykoor', true],
+];
 
-global.mods = []
-global.prems = []
+//*─ׄ─⋯
 
-const packname = `✨Anika🌸`
-const author = '🌃Neykoor✨'
-const wait = '🌹Anika✨'
-const botname = '🌸Anika✨'
-const textbot = `🌸Anika a tu servicio✨`
-const listo = 'Anika lista para servirle🌹!'
-const namechannel = '「  Anika-news  」'
-const baileys = '@whiskeysockets/baileys'
+global.mods = ['5216677510398'], 
+global.suittag = ['']
+global.prems = ['5216671548329'], 
 
-const catalogoPath = './storage/img/catalogo.png'
-const siskedurlPath = './storage/img/siskedurl.jpg'
+//*─ׄ─⋯
 
-global.catalogo = fs.existsSync(catalogoPath) ? fs.readFileSync(catalogoPath) : null
-global.siskedurl = fs.existsSync(siskedurlPath) ? fs.readFileSync(siskedurlPath) : null
+global.libreria = 'Baileys'
+global.baileys = 'V 6.7.9' 
+global.languaje = 'Español'
+global.vs = '2.1.5'
+global.vsJB = '5.0'
+global.nameqr = 'Sagiri - Izumi - Bot'
+global.namebot = '✿◟Sagiri-izumi◞✿'
+global.sessions = 'Sessions'
+global.jadi = 'JadiBots' 
+global.yukiJadibts = true
 
-global.group = 'https://chat.whatsapp.com/LcweEoBy4W16ngE2Pr4ClR'
-global.canal = 'https://whatsapp.com/channel/0029Vb3uTsb90x2rvI6D3G3b'
+//*─ׄ─⋯
 
-global.estilo = {
-  key: {
-    fromMe: false,
-    participant: `0@s.whatsapp.net`,
-    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {})
-  },
-  message: {
-    orderMessage: {
-      itemCount: -999999,
-      status: 1,
-      surface: 1,
-      message: botname,
-      orderTitle: 'Bang',
-      thumbnail: global.catalogo,
-      sellerJid: '0@s.whatsapp.net'
-    }
-  }
+global.packsticker =`°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n✦ Bot:\n✦ Propietario:\n✦Fecha de Creacion:\n✦ Hora de creación:\n♾━━━━━━━━`
+global.packsticker2 = `━━━━━━━━♾\n⪛✰ sᥲgіrі - mіzᥙkі ✰⪜\nLegna\n⇝ ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}\n⇝ ${moment.tz('America/Los_Angeles').format('HH:mm:ss')} \n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n\nStickers By : Legna`
+
+//*─ׄ─⋯
+
+global.packname = 'Sagiri Izumi'
+global.botname = 'Sagiri Izumi'
+global.wm = 'Sagiri Izumi'
+global.author = 'Made By Legna'
+global.dev = 'Powered By Legna'
+global.textbot = '❀ sᥲgіrі іzᥙmі ᑲᥡ : ᥣᥱgᥒᥲ ❀'
+global.numcreador = '5216671548329'
+global.moneda = 'SagiriCoins'
+global.banner = 'https://files.catbox.moe/l3edoq.jpg'
+global.catalogo = 'https://files.catbox.moe/gizmm4.jpg'
+global.test = 'https://qu.ax/zppMw.jpg'
+
+//*─ׄ─⋯
+
+global.imagen1 = fs.readFileSync('./src/menus/Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/anime.jpg');
+global.imagen3 = fs.readFileSync('./src/menus/Menu3.jpg');
+global.imagen4 = fs.readFileSync('./src/menus/Menu.jpg');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./src/menus/Menu3.jpg');
+global.imagen7 = fs.readFileSync('./src/menus/Menu5.jpg');
+global.imagen8 = fs.readFileSync('./src/menus/Menu4.jpg')
+global.imagen9 = fs.readFileSync('./src/menu_en.png')
+global.imagen10 = fs.readFileSync('./src/nuevobot.jpg');
+global.miniurl = fs.readFileSync('./src/Grupo.jpg');
+global.logo2 = fs.readFileSync('./src/logo2.jpg')
+global.logo3 = fs.readFileSync('./src/logo3.jpg')
+global.catalogo = fs.readFileSync('./src/logo6.png')
+global.logo4 = fs.readFileSync('./src/logo4.jpg')
+global.logo5 = fs.readFileSync('./src/logo5.jpg')
+global.logo7 = fs.readFileSync('./src/Logo7.png')
+global.logo8 = fs.readFileSync('./src/Logo8.jpg')
+global.rule = fs.readFileSync('./src/rule.jpg')
+
+global.photoSity = [imagen8, imagen1, imagen4, imagen6]
+
+//*─ׄ─⋯
+
+global.gp4 = 'https://chat.whatsapp.com/Ecz881bBgqPIWjDOaKkp7E'
+global.gp1 = 'https://chat.whatsapp.com/D8U3fQB1oLj3TzqwhRmzAD' 
+global.gp2 = 'https://chat.whatsapp.com/D8U3fQB1oLj3TzqwhRmzAD'
+global.comunidad1 = 'https://chat.whatsapp.com/EBwwYcU2AzC1vqVhVUgJ3o'
+global.channel = 'https://whatsapp.com/channel/0029Vb3yy64LI8YSrvdyq00R'
+global.channel2 = 'https://whatsapp.com/channel/0029VavzewJLikg78gILRn1o'
+global.md = 'https://github.com/Legna-chan/SagiriBot-MD'
+global.correo = 'legnakujou@gmail.com'
+global.cn ='https://whatsapp.com/channel/0029VapSIvR5EjxsD1B7hU3T';
+
+//*─ׄ─⋯
+
+global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: '❀ sᥲgіrі іzᥙmі ☄︎︎', orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+
+//*─ׄ─⋯
+
+global.ch = {
+ch1: '120363390725729031@newsletter',
+ch2: '120363390725729031@newsletter',
 }
+
+//*─ׄ─⋯
 
 global.cheerio = cheerio
 global.fs = fs
 global.fetch = fetch
 global.axios = axios
-global.moment = moment	
+global.moment = moment   
 
-global.multiplier = 69 
-global.maxwarn = '2'
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emot = {
+      level: '🌟 Nivel',
+      coin: '💸 Coin',
+      exp: '✨ Experiencia',
+      bank: '🏦 Banco',
+      diamond: '💎 Diamante',
+      health: '❤️ Salud',
+      kyubi: '🌀 Magia',
+      joincount: '💰 Token',
+      emerald: '♦️ Esmeralda',
+      stamina: '⚡ Energía',
+      role: '⚜️ Rango',
+      premium: '🎟️ Premium',
+      pointxp: '📧 Puntos Exp',
+      gold: '👑 Oro',
+      iron: '⛓️ Hierro',
+      coal: '🌑 Carbón',
+      stone: '🪨 Piedra',
+      potion: '🥤 Poción',
+    };
+    const results = Object.keys(emot).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emot[results[0][0]];
+  }};
+global.rpgg = { 
+  emoticon(string) {
+    string = string.toLowerCase();
+    const emott = {
+      level: '🌟',
+      coin: '💸',
+      exp: '✨',
+      bank: '🏦',
+      diamond: '💎',
+      health: '❤️',
+      kyubi: '🌀',
+      joincount: '💰',
+      emerald: '♦️',
+      stamina: '⚡',
+      role: '⚜️',
+      premium: '🎟️',
+      pointxp: '📧',
+      gold: '👑',
+      iron: '⛓️',
+      coal: '🌑',
+      stone: '🪨',
+      potion: '🥤',
+    };
+    const results = Object.keys(emott).map((v) => [v, new RegExp(v, 'gi')]).filter((v) => v[1].test(string));
+    if (!results.length) return '';
+    else return emott[results[0][0]];
+  }};  
+
+//*─ׄ─⋯
+
+global.multiplier = 69
+global.maxwarn = '3'
+
+//*─ׄ─⋯
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
   console.log(chalk.redBright("Update 'config.js'"))
-  import(file).then(() => console.log(chalk.greenBright('Config.js actualizado!')))
+  import(`${file}?update=${Date.now()}`)
 })
-
-export { packname, author, wait, botname, textbot, listo, namechannel, baileys };
