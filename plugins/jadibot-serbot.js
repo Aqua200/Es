b94294ce5177182324fdb5b4d6d99812a14b7b25
Aqua200 <chinquepapa@gmail.com>
2025-02-12 07:01:42 -0800

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,33 +1,31 @@
 // jadibot-serbot.js
-export default function yukiJadiBot() {
+function yukiJadiBot() {
   // código de la función...
 }
+
 import fs from 'fs';
 import path from 'path';
 
 const loadPlugin = (pluginName) => {
-    // Asegúrate de que la ruta es correcta y que el archivo tiene la extensión .js
-    const pluginPath = path.resolve(`/data/data/com.termux/files/home/ri/plugins/${pluginName}.js`);
-    
-    return new Promise((resolve, reject) => {
-        // Verificar si el archivo existe en la ruta especificada
-        if (fs.existsSync(pluginPath)) {
-            // Intentar importar el plugin
-            import(pluginPath)
-                .then((plugin) => {
-                    console.log(`Plugin ${pluginName} cargado correctamente.`);
-                    resolve(plugin);
-                })
-                .catch((error) => {
-                    console.error(`Error al cargar el plugin ${pluginName}:`, error);
-                    reject(error);
-                });
-        } else {
-            const errorMsg = `El archivo ${pluginName}.js no se encuentra en la ruta especificada.`;
-            console.error(errorMsg);
-            reject(new Error(errorMsg));
-        }
-    });
+  const pluginPath = path.resolve(`/data/data/com.termux/files/home/ri/plugins/${pluginName}.js`);
+
+  return new Promise((resolve, reject) => {
+      if (fs.existsSync(pluginPath)) {
+          import(pluginPath)
+              .then((plugin) => {
+                  console.log(`Plugin ${pluginName} cargado correctamente.`);
+                  resolve(plugin);
+              })
+              .catch((error) => {
+                  console.error(`Error al cargar el plugin ${pluginName}:`, error);
+                  reject(error);
+              });
+      } else {
+          const errorMsg = `El archivo ${pluginName}.js no se encuentra en la ruta especificada.`;
+          console.error(errorMsg);
+          reject(new Error(errorMsg));
+      }
+  });
 };
 
-export default loadPlugin;
+export { yukiJadiBot, loadPlugin };
