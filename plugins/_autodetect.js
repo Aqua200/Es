a00b5b4303b3cb2d16a94fe3d2e083733b93069f
Aqua200 <chinquepapa@gmail.com>
2025-02-12 00:26:42 -0800

    Update _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -7,16 +7,14 @@ let chat = global.db.data.chats[m.chat]
 let usuario = `@${m.sender.split`@`[0]}`
 let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'  
 
-let nombre, foto, edit, newlink, status, admingp, noadmingp, aceptar
-nombre = `《✧》${usuario} Ha cambiado el nombre del grupo.\n\n> ✦ Ahora el grupo se llama:\n> *${m.messageStubParameters[0]}*.`
-foto = `《✧》${usuario} Ha cambiado la imagen del grupo.`
-edit = `《✧》${usuario} Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`
-newlink = `《✧》El enlace del grupo ha sido restablecido.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-status = `《✧》El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por ${usuario}\n\n> ✦ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje.`
-admingp = `《✧》@${m.messageStubParameters[0].split`@`[0]} Ahora es admin del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-noadmingp =  `《✧》@${m.messageStubParameters[0].split`@`[0]} Deja de ser admin del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
-aceptar = `《✧》Ha llegado un nuevo participante al grupo.\n\n> ◦ ✐ Grupo: *${groupMetadata.subject}*\n\n> ◦ ⚘ Bienvenido/a: @${m.messageStubParameters[0].split('@')[0]}\n\n> ◦ ✦ Aceptado por:
- @${m.sender.split('@')[0]}` 
+let nombre, foto, edit, newlink, status, admingp, noadmingp
+nombre = `*${usuario}*\n🍡 Ha cambiado el nombre del grupo.\n\n🪷 Ahora el grupo se llama:\n*<${m.messageStubParameters[0]}>*...`
+foto = `*${usuario}*\n🍡 Ha cambiado la imagen del grupo...`
+edit = `*${usuario}*\n🪷 Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo...`
+newlink = `🍡 El enlace del grupo ha sido restablecido por:\n*» ${usuario}*...`
+status = `🍡 El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado 🔒*' : '*abierto 🔓*'} Por *${usuario}*\n\n🪷 Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje...`
+admingp = `*@${m.messageStubParameters[0].split`@`[0]}* Ahora es admin del grupo 🦄\n\n🍡 Acción hecha por:\n*» ${usuario}*...`
+noadmingp =  `*@${m.messageStubParameters[0].split`@`[0]}* Deja de ser admin del grupo 🦄\n\n🍡 Acción hecha por:\n*» ${usuario}*...`
 
 if (chat.detect && m.messageStubType == 21) {
 await conn.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })   
@@ -32,9 +30,6 @@ await conn.sendMessage(m.chat, { text: edit, mentions: [m.sender] }, { quoted: f
 
 } else if (chat.detect && m.messageStubType == 26) {
 await conn.sendMessage(m.chat, { text: status, mentions: [m.sender] }, { quoted: fkontak })  
-
-} else if (chat.detect2 && m.messageStubType == 27) {
-await conn.sendMessage(m.chat, { text: aceptar, mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`] }, { quoted: fkontak })
 
 } else if (chat.detect && m.messageStubType == 29) {
 await conn.sendMessage(m.chat, { text: admingp, mentions: [`${m.sender}`,`${m.messageStubParameters[0]}`] }, { quoted: fkontak })  
